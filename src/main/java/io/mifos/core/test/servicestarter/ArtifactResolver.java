a78edf3fc83148dc3dba4e180bedf7405a173d15
myrle-krantz <mkrantz@mifos.org>
2017-03-15 18:40:35 +0100

    Open Sourcing service-starter.

---
+++ src/main/java/io/mifos/core/test/servicestarter/ArtifactResolver.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 The Mifos Initiative.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mifos.core.test.servicestarter;
+
+import io.mifos.core.test.servicestarter.aether.Booter;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
+
+import java.io.File;
+import java.util.Collections;
+
+/**
+ * @author Myrle Krantz
+ */
+class ArtifactResolver {
+  private final String artifactoryDirectory;
+  private final String username;
+  private final String password;
+
+  private RemoteRepository repositoryForProject(final String project)
+  {
+    return new RemoteRepository.Builder(project, "default", "https://mifosio-radar.bintray.com/io.mifos." + project)
+            .setAuthentication(new AuthenticationBuilder().addUsername(username).addPassword(password).build())
+            .build();
+  }
+
+
+  ArtifactResolver(final String artifactoryDirectory, final String username, final String password)
+  {
+    this.artifactoryDirectory = artifactoryDirectory;
+    this.username = username;
+    this.password = password;
+  }
+
+  File getJarFile(final String project, final String group, final String name, final String version)
+          throws ArtifactResolutionException {
+    final RepositorySystem system = Booter.newRepositorySystem();
+    final RepositorySystemSession session = Booter.newRepositorySystemSession(system, artifactoryDirectory);
+
+
+    final ArtifactRequest artifactRequest = new ArtifactRequest();
+    artifactRequest.setArtifact(new DefaultArtifact(group, name, "jar", version));
+    artifactRequest.setRepositories(Collections.singletonList(repositoryForProject(project)));
+
+    final ArtifactResult artifactResult = system.resolveArtifact(session, artifactRequest);
+
+    return artifactResult.getArtifact().getFile();
+  }
+}
